ef85b04fcbf6376aaec0802b65c5c7b217eb0639
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-09-30 20:41:05 +0530

    changes in dataTypes.js

--- Basics/dataTypes.js
+++ Basics/dataTypes.js
@@ -5,9 +5,11 @@ let salary = 4500.89;
 let name = "Rajesh";
 let isLoggedIn = false;
 
-// number , string, boolean, null -> type object , undefined -> type undefined ,   -> primitive
+// number , string, boolean, null , undefined ,   -> primitive
 // symbol->unique identifier    -> primitive
 
 // object -> non primitive -> array, function, object
 
 console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object -> bug in js
