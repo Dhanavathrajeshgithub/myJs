f8b2d6800a363a65796d69132c21125a53ed12c6
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-10-01 15:13:33 +0530

    added comparisions.js file

---
+++ Basics/05_comparisions.js
@@ -0,0 +1,18 @@
+// console.log(1 > 2);
+// console.log(10 < 2);
+// console.log(1 != 2);
+// console.log(1 >= 2);
+// console.log(1 <= 2);
+
+//console.log("8347" > 3837855); // false
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); //  true
+
+//  **************    NOTE    **************
+// comparision operator > >= < <= converts null to a number to 0
+// Equality works different .
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false
