1d2f173bf9d1e41741b97d78642cc053fdab650d
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-10-01 14:52:28 +0530

    operations.js

---
+++ Basics/operations.js
@@ -0,0 +1,25 @@
+let val = 3;
+let negVal = -val;
+//console.log(negVal);
+/*
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 4);
+console.log(2 / 4);
+console.log(2 % 4);
+*/
+
+let str1 = "Hello ",
+  str2 = "Rajesh";
+let str3 = str1 + str2;
+//console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log(1 + 2 + "2");  // 32
+// console.log("1" + 2 + 2);  // 122
+
+console.log(+true); // 1
+console.log(+""); // 0
