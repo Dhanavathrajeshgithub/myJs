73b93fabaffd488770dfdf56402f9b0e57c0ce63
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-10-06 21:01:11 +0530

    Code execution

---
+++ 03_Basics/06_exeCode.js
@@ -0,0 +1,20 @@
+/*
+# JavaScript code execution
+
+    Given Js code -> 1) Global Execution context (GEC) -> this  ,   in browser -> window
+    2) function execution context
+    3) eval execution context
+
+    >> Two Phases
+       1) Creation Phase / Memory Creation Phase
+       2) Execution Phase / Code Execution Phase 
+
+
+
+
+
+
+
+
+
+*/
