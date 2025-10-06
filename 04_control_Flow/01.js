1ff31ac021e9bd88199f892fecfc2225075001a7
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-10-06 21:26:44 +0530

    If condition

---
+++ 04_control_Flow/01.js
@@ -0,0 +1,17 @@
+// IF
+let condition = true;
+// if (condition) {
+//   console.log("Executed");
+// }
+
+// if (2 == "2") {
+//   // true
+// }
+// if (2 === "2") {
+//   // false;
+// }
+
+if (condition) console.log("execute"); // only one line
+
+// if (condition)
+//   console.log("line 1"), console.log("line 2"), console.log("line3 "); // bad practice but works
