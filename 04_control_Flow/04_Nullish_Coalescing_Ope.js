59cc058e5aac19bbf3dda6b91b3b938a26f90589
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-10-06 22:07:10 +0530

    Nullish Coalescing operator

---
+++ 04_control_Flow/04_Nullish_Coalescing_Ope.js
@@ -0,0 +1,8 @@
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+// val1 = 5 ?? 10;  // 5
+// val1 = null ?? 10; // 10
+// val1 = undefined ?? 10; // 10
+// val1 = null ?? undefined ?? 10;  // 10
+console.log(val1);
